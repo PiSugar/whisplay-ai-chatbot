9176df5c1fd1d3166b4d09f5181a5577a736404b
Jdaie <490844515@qq.com>
2025-10-23 11:57:04 +0800

    feat: find soundcard index in start script

--- run_chatbot.sh
+++ run_chatbot.sh
@@ -4,11 +4,14 @@ export NVM_DIR="/home/pi/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-SOUND_CARD_INDEX=1
-# if Raspberry Pi 4 or 5, set SOUND_CARD_INDEX=2
-if grep -q "Raspberry Pi 4" /proc/device-tree/model || grep -q "Raspberry Pi 5" /proc/device-tree/model; then
-  SOUND_CARD_INDEX=2
+# Find the sound card index for wm8960soundcard
+card_index=$(awk '/wm8960soundcard/ {print $1}' /proc/asound/cards | head -n1)
+# Default to 1 if not found
+if [ -z "$card_index" ]; then
+  card_index=1
 fi
+echo "$card_index"
+
 # Output current environment information (for debugging)
 echo "===== Start time: $(date) =====" 
 echo "Current user: $(whoami)" 
@@ -18,10 +21,10 @@ echo "Python version: $(python3 --version)"
 echo "Node version: $(node --version)"
 sleep 5
 # Adjust volume
-amixer -c $SOUND_CARD_INDEX set Speaker 114
+amixer -c $card_index set Speaker 114
 # Start the service
 echo "Starting Node.js application..."
 cd /home/pi/whisplay-ai-chatbot
-SOUND_CARD_INDEX=$SOUND_CARD_INDEX yarn start
+SOUND_CARD_INDEX=$card_index yarn start
 # Record end status
 echo "===== Service ended: $(date) ====="
