f31499455ff3a802e9c02ad541ca61b1bdc8fa9c
Jdaie <490844515@qq.com>
2025-11-02 15:36:55 +0800

    feat: picam use cv2

--- python/picam-test.py
+++ python/picam-test.py
@@ -1,8 +1,6 @@
 from picamera2 import Picamera2
-from PIL import Image, ImageDraw, ImageFont
 import cv2
 from whisplay import WhisplayBoard
-from utils import ColorUtils, ImageUtils, TextUtils
 import sys
 import time
 import numpy as np
@@ -18,22 +16,24 @@ if __name__ == "__main__":
         whisplay.cleanup()
         sys.exit(0)
         
-    # def bgr888_to_rgb565(frame, buf):
-    #     # use opencv to convert BGR888 to RGB565
-    #     b, g, r = cv2.split(frame)
-    #     r = (r >> 3).astype(np.uint16)
-    #     g = (g >> 2).astype(np.uint16)
-    #     b = (b >> 3).astype(np.uint16)
-    #     np.bitwise_or(r << 11, g << 5, out=buf)
-    #     np.bitwise_or(buf, b, out=buf)
-    #     return buf.byteswap().tobytes()
-        
     picam2 = Picamera2()
-    picam2.configure(picam2.create_preview_configuration(main={"size": (whisplay.LCD_WIDTH, whisplay.LCD_HEIGHT)}))
+    picam2.configure(picam2.create_preview_configuration(main={"size": (480, 560)}))
     picam2.start()
     whisplay.set_backlight(100)
-    rgb565_buf = np.empty((whisplay.LCD_HEIGHT, whisplay.LCD_WIDTH), dtype=np.uint16)
     time.sleep(2)  # Allow camera to warm up
+
+    rgb565_buf = np.zeros((whisplay.LCD_WIDTH * whisplay.LCD_HEIGHT), dtype=np.uint16)
+
+    def convertCameraFrameToRGB565(frame, width, height, rgb565):
+        # Resize frame to fit the display
+        frame = cv2.resize(frame, (width, height), interpolation=cv2.INTER_NEAREST)
+        r = (frame[:, :, 0] >> 3).astype(np.uint16)  # 5 bit
+        g = (frame[:, :, 1] >> 2).astype(np.uint16)  # 6 bit
+        b = (frame[:, :, 2] >> 3).astype(np.uint16)  # 5 bit
+        rgb565_data = (r << 11) | (g << 5) | b
+        np.copyto(rgb565, rgb565_data)
+        return rgb565.byteswap().tobytes()
+
     try:
         
         # Keep the main thread alive
@@ -41,15 +41,7 @@ if __name__ == "__main__":
             # Capture image from Pi Camera
             start_time = time.time()
             frame = picam2.capture_array()
-            # img = img.resize((whisplay.LCD_WIDTH, whisplay.LCD_HEIGHT), Image.LANCZOS)
-            # Convert the image to RGB565 format
-            # pixel_data = bgr888_to_rgb565(frame, rgb565_buf)
-            frame = cv2.resize(frame, (240, 280), interpolation=cv2.INTER_NEAREST)
-            r = (frame[:, :, 0] >> 3).astype(np.uint16)  # 5 bit
-            g = (frame[:, :, 1] >> 2).astype(np.uint16)  # 6 bit
-            b = (frame[:, :, 2] >> 3).astype(np.uint16)  # 5 bit
-            rgb565 = (r << 11) | (g << 5) | b
-            pixel_bytes = rgb565.byteswap().tobytes()
+            pixel_bytes = convertCameraFrameToRGB565(frame, whisplay.LCD_WIDTH, whisplay.LCD_HEIGHT, rgb565_buf)
             
             # Send the pixel data to the display
             whisplay.draw_image(0, 0, whisplay.LCD_WIDTH, whisplay.LCD_HEIGHT, pixel_bytes)
