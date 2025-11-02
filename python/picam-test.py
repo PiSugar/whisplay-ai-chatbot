df4c1898d43a8bfa5692bffd63f7db0301cc1b00
Jdaie <490844515@qq.com>
2025-11-02 15:37:58 +0800

    feat: picam use cv2

--- python/picam-test.py
+++ python/picam-test.py
@@ -22,17 +22,14 @@ if __name__ == "__main__":
     whisplay.set_backlight(100)
     time.sleep(2)  # Allow camera to warm up
 
-    rgb565_buf = np.zeros((whisplay.LCD_WIDTH * whisplay.LCD_HEIGHT), dtype=np.uint16)
-
-    def convertCameraFrameToRGB565(frame, width, height, rgb565):
+    def convertCameraFrameToRGB565(frame, width, height):
         # Resize frame to fit the display
         frame = cv2.resize(frame, (width, height), interpolation=cv2.INTER_NEAREST)
         r = (frame[:, :, 0] >> 3).astype(np.uint16)  # 5 bit
         g = (frame[:, :, 1] >> 2).astype(np.uint16)  # 6 bit
         b = (frame[:, :, 2] >> 3).astype(np.uint16)  # 5 bit
         rgb565_data = (r << 11) | (g << 5) | b
-        np.copyto(rgb565, rgb565_data)
-        return rgb565.byteswap().tobytes()
+        return rgb565_data.byteswap().tobytes()
 
     try:
         
@@ -41,7 +38,7 @@ if __name__ == "__main__":
             # Capture image from Pi Camera
             start_time = time.time()
             frame = picam2.capture_array()
-            pixel_bytes = convertCameraFrameToRGB565(frame, whisplay.LCD_WIDTH, whisplay.LCD_HEIGHT, rgb565_buf)
+            pixel_bytes = convertCameraFrameToRGB565(frame, whisplay.LCD_WIDTH, whisplay.LCD_HEIGHT)
             
             # Send the pixel data to the display
             whisplay.draw_image(0, 0, whisplay.LCD_WIDTH, whisplay.LCD_HEIGHT, pixel_bytes)
