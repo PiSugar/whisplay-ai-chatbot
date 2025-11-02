2f584d8505ad84d562e7c276ec4e21de2bd24a08
Jdaie <490844515@qq.com>
2025-11-02 15:10:33 +0800

    feat: picam use cv2

--- python/picam-test.py
+++ python/picam-test.py
@@ -1,5 +1,6 @@
 from picamera2 import Picamera2
 from PIL import Image, ImageDraw, ImageFont
+import cv2
 from whisplay import WhisplayBoard
 from utils import ColorUtils, ImageUtils, TextUtils
 import sys
@@ -17,11 +18,12 @@ if __name__ == "__main__":
         whisplay.cleanup()
         sys.exit(0)
         
-    def rgb888_to_565_inplace(frame, buf):
-        """快速将 RGB888 转 RGB565，结果写入 buf (np.uint16)"""
-        r = (frame[..., 0] >> 3).astype(np.uint16)
-        g = (frame[..., 1] >> 2).astype(np.uint16)
-        b = (frame[..., 2] >> 3).astype(np.uint16)
+    def bgr888_to_rgb565(frame, buf):
+        # use opencv to convert BGR888 to RGB565
+        b, g, r = cv2.split(frame)
+        r = (r >> 3).astype(np.uint16)
+        g = (g >> 2).astype(np.uint16)
+        b = (b >> 3).astype(np.uint16)
         np.bitwise_or(r << 11, g << 5, out=buf)
         np.bitwise_or(buf, b, out=buf)
         return buf.byteswap().tobytes()
@@ -39,10 +41,9 @@ if __name__ == "__main__":
             # Capture image from Pi Camera
             print("[Camera] Capturing frame...")
             frame = picam2.capture_array()
-            img = Image.fromarray(frame)
             # img = img.resize((whisplay.LCD_WIDTH, whisplay.LCD_HEIGHT), Image.LANCZOS)
             # Convert the image to RGB565 format
-            pixel_data = ImageUtils.image_to_rgb565(img, whisplay.LCD_WIDTH, whisplay.LCD_HEIGHT)
+            pixel_data = bgr888_to_rgb565(frame, rgb565_buf)
             # Draw the image on the LCD
             whisplay.draw_image(0, 0, whisplay.LCD_WIDTH, whisplay.LCD_HEIGHT, pixel_data)
 
