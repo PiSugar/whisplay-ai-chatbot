c3876a231b0add37bd64ec663320ad57cbbc6885
Jdaie <490844515@qq.com>
2025-11-02 15:12:54 +0800

    feat: picam use cv2

--- python/picam-test.py
+++ python/picam-test.py
@@ -18,15 +18,15 @@ if __name__ == "__main__":
         whisplay.cleanup()
         sys.exit(0)
         
-    def bgr888_to_rgb565(frame, buf):
-        # use opencv to convert BGR888 to RGB565
-        b, g, r = cv2.split(frame)
-        r = (r >> 3).astype(np.uint16)
-        g = (g >> 2).astype(np.uint16)
-        b = (b >> 3).astype(np.uint16)
-        np.bitwise_or(r << 11, g << 5, out=buf)
-        np.bitwise_or(buf, b, out=buf)
-        return buf.byteswap().tobytes()
+    # def bgr888_to_rgb565(frame, buf):
+    #     # use opencv to convert BGR888 to RGB565
+    #     b, g, r = cv2.split(frame)
+    #     r = (r >> 3).astype(np.uint16)
+    #     g = (g >> 2).astype(np.uint16)
+    #     b = (b >> 3).astype(np.uint16)
+    #     np.bitwise_or(r << 11, g << 5, out=buf)
+    #     np.bitwise_or(buf, b, out=buf)
+    #     return buf.byteswap().tobytes()
         
     picam2 = Picamera2()
     picam2.configure(picam2.create_preview_configuration(main={"size": (whisplay.LCD_WIDTH, whisplay.LCD_HEIGHT)}))
@@ -43,9 +43,11 @@ if __name__ == "__main__":
             frame = picam2.capture_array()
             # img = img.resize((whisplay.LCD_WIDTH, whisplay.LCD_HEIGHT), Image.LANCZOS)
             # Convert the image to RGB565 format
-            pixel_data = bgr888_to_rgb565(frame, rgb565_buf)
+            # pixel_data = bgr888_to_rgb565(frame, rgb565_buf)
+            frame = cv2.resize(frame, (240, 280), interpolation=cv2.INTER_NEAREST)
+            rgb565 = np.frombuffer(frame, dtype=np.uint16)
             # Draw the image on the LCD
-            whisplay.draw_image(0, 0, whisplay.LCD_WIDTH, whisplay.LCD_HEIGHT, pixel_data)
+            whisplay.draw_image(0, 0, whisplay.LCD_WIDTH, whisplay.LCD_HEIGHT, rgb565)
 
             time.sleep(0.1)  # Adjust the delay as needed for your application
     except KeyboardInterrupt:
