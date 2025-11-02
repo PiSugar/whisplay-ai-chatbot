00ed761d74440dbcaa59ab7465bd88d97806827d
Jdaie <490844515@qq.com>
2025-11-02 14:39:54 +0800

    feat: picam test

--- python/picam-test.py
+++ python/picam-test.py
@@ -1,5 +1,7 @@
 from picamera2 import Picamera2
+from PIL import Image, ImageDraw, ImageFont
 from whisplay import WhisplayBoard
+from utils import ColorUtils, ImageUtils, TextUtils
 import sys
 import time
 
@@ -14,33 +16,27 @@ if __name__ == "__main__":
         whisplay.cleanup()
         sys.exit(0)
         
+    picam2 = Picamera2()
+    picam2.configure(picam2.create_preview_configuration(main={"format": "RGB888", "size": (whisplay.LCD_WIDTH, whisplay.LCD_HEIGHT)}))
+    picam2.start()
+    time.sleep(2)  # Allow camera to warm up
     try:
+        
         # Keep the main thread alive
         while True:
             # Capture image from Pi Camera
-            picam2 = Picamera2()
-            picam2.configure(picam2.create_preview_configuration(main={"format": "RGB888", "size": (whisplay.LCD_WIDTH, whisplay.LCD_HEIGHT)}))
-            picam2.start()
-            time.sleep(2)  # Allow camera to warm up
+            
             frame = picam2.capture_array()
-            picam2.stop()
-
+            img = Image.fromarray(frame)
+            img = img.resize((whisplay.LCD_WIDTH, whisplay.LCD_HEIGHT))
             # Convert the image to RGB565 format
-            pixel_data = []
-            for y in range(whisplay.LCD_HEIGHT):
-                for x in range(whisplay.LCD_WIDTH):
-                    r = frame[y, x, 0] >> 3
-                    g = frame[y, x, 1] >> 2
-                    b = frame[y, x, 2] >> 3
-                    rgb565 = (r << 11) | (g << 5) | b
-                    pixel_data.append((rgb565 >> 8) & 0xFF)
-                    pixel_data.append(rgb565 & 0xFF)
-
+            pixel_data = ImageUtils.convert_image_to_rgb565(img)
             # Draw the image on the LCD
             whisplay.draw_image(0, 0, whisplay.LCD_WIDTH, whisplay.LCD_HEIGHT, pixel_data)
 
             # time.sleep(0.1)  # Adjust the delay as needed for your application
     except KeyboardInterrupt:
+        picam2.stop()
         cleanup_and_exit(None, None)
 
 
