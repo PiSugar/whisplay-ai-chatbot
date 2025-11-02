9a72227c5a7786adc17bcdaf9f5f426329115ecd
Jdaie <490844515@qq.com>
2025-11-02 14:54:15 +0800

    feat: picam test

--- python/picam-test.py
+++ python/picam-test.py
@@ -4,6 +4,7 @@ from whisplay import WhisplayBoard
 from utils import ColorUtils, ImageUtils, TextUtils
 import sys
 import time
+import numpy as np
 
 
 if __name__ == "__main__":
@@ -16,10 +17,20 @@ if __name__ == "__main__":
         whisplay.cleanup()
         sys.exit(0)
         
+    def rgb888_to_565_inplace(frame, buf):
+        """快速将 RGB888 转 RGB565，结果写入 buf (np.uint16)"""
+        r = (frame[..., 0] >> 3).astype(np.uint16)
+        g = (frame[..., 1] >> 2).astype(np.uint16)
+        b = (frame[..., 2] >> 3).astype(np.uint16)
+        np.bitwise_or(r << 11, g << 5, out=buf)
+        np.bitwise_or(buf, b, out=buf)
+        return buf.byteswap().tobytes()
+        
     picam2 = Picamera2()
-    picam2.configure(picam2.create_preview_configuration(main={"size": (whisplay.LCD_WIDTH, whisplay.LCD_HEIGHT)}))
+    picam2.configure(picam2.create_preview_configuration(main={"size": (whisplay.LCD_WIDTH, whisplay.LCD_HEIGHT), "format": "RGB888"}))
     picam2.start()
     whisplay.set_backlight(100)
+    rgb565_buf = np.empty((whisplay.LCD_HEIGHT, whisplay.LCD_WIDTH), dtype=np.uint16)
     time.sleep(2)  # Allow camera to warm up
     try:
         
@@ -28,10 +39,12 @@ if __name__ == "__main__":
             # Capture image from Pi Camera
             print("[Camera] Capturing frame...")
             frame = picam2.capture_array()
-            img = Image.fromarray(frame)
-            img = img.resize((whisplay.LCD_WIDTH, whisplay.LCD_HEIGHT), Image.LANCZOS)
+            # img = Image.fromarray(frame)
+            # img = img.resize((whisplay.LCD_WIDTH, whisplay.LCD_HEIGHT), Image.LANCZOS)
             # Convert the image to RGB565 format
-            pixel_data = ImageUtils.image_to_rgb565(img, whisplay.LCD_WIDTH, whisplay.LCD_HEIGHT)
+            # pixel_data = ImageUtils.image_to_rgb565(img, whisplay.LCD_WIDTH, whisplay.LCD_HEIGHT)
+            
+            pixel_data = rgb888_to_565_inplace(frame, rgb565_buf)
             # Draw the image on the LCD
             whisplay.draw_image(0, 0, whisplay.LCD_WIDTH, whisplay.LCD_HEIGHT, pixel_data)
 
