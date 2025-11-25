b3d823fa0cd54bef1d57847c575ca680b7bf7bc3
Jdaie <490844515@qq.com>
2025-11-25 18:56:45 +0800

    fix: test play

---
+++ src/test/audio-play.ts
@@ -0,0 +1,32 @@
+import { ttsDir } from "../utils/dir";
+
+// list all wav files in ttsDir
+import fs from "fs";
+import path from "path";
+import { playAudioData } from "../device/audio";
+import getAudioDurationInSeconds from "get-audio-duration";
+
+export const listWavFilesInTtsDir = (): string[] => {
+  if (!fs.existsSync(ttsDir)) {
+    return [];
+  }
+  const files = fs.readdirSync(ttsDir);
+  return files
+    .filter((file) => file.endsWith(".wav"))
+    .map((file) => path.join(ttsDir, file));
+};
+
+const files = listWavFilesInTtsDir();
+
+const playAllWavFiles = async () => {
+  for (const filePath of files) {
+    console.log("Playing:", filePath);
+    const buffer = fs.readFileSync(filePath);
+    const duration = (await getAudioDurationInSeconds(filePath)) * 1000;
+    const headerSize = 44;
+    const trimmedBuffer = buffer.subarray(headerSize);
+    await playAudioData(trimmedBuffer.toString("base64"), duration);
+  }
+};
+
+playAllWavFiles();
