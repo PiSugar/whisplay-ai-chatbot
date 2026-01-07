6f8ffccd1e015ee2e6f30e1b3e45b701d46f60bc
Jdaie <490844515@qq.com>
2026-01-07 09:10:10 +0800

    fix: install dep overwrite

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -28,8 +28,8 @@ echo "Installing Python dependencies..."
 cd python
 pip install -r requirements.txt --break-system-packages
 # download fonts and emojis
-wget https://cdn.pisugar.com/EchoView/NotoSansSC-Bold.ttf
-wget https://cdn.pisugar.com/EchoView/emoji_svg.zip
+wget -O NotoSansSC-Bold.ttf https://cdn.pisugar.com/EchoView/NotoSansSC-Bold.ttf
+wget -O emoji_svg.zip https://cdn.pisugar.com/EchoView/emoji_svg.zip
 # overwrite if exists
 unzip -o emoji_svg.zip
 cd ..
